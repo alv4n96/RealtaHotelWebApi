05c644ba63d36c13f4656d075d8a9105d434addd
alvan <alvan.ghon@gmail.com>
2023-02-13 23:10:56 +0700

    fixing Hotel entity

--- Realta.Domain/Entities/Hotels.cs
+++ Realta.Domain/Entities/Hotels.cs
@@ -8,5 +8,12 @@ namespace Realta.Domain.Entities
 {
     internal class Hotels
     {
+        public int hotel_id { get; set; }
+        public string hotel_name { get; set; }
+        public string hotel_description { get; set; }
+        public UInt16 hotel_rating_star { get; set; }
+        public string hotel_phonenumber { get; set; }
+        public DateTime hotel_modified_date { get; set; } = DateTime.Now;
+        public int hotel_addr_id { get; set; } 
     }
 }
