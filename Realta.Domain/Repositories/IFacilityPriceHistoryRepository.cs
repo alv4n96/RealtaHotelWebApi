f063c486727a2bb3d8ba343b264e6f71f987b45c
alvan <alvan.ghon@gmail.com>
2023-02-23 02:38:36 +0700

    Interface Facility Price History

---
+++ Realta.Domain/Repositories/IFacilityPriceHistoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using Realta.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Realta.Domain.Repositories
+{
+    public interface IFacilityPriceHistoryRepository
+    {
+        Task<IEnumerable<Facility_Price_History>> FindAllFacilityPriceHistoryAsync(int hotelId);
+        Task<IEnumerable<Facility_Price_History>> FindAllFacilityPriceHistoryByFacilityAsync(int hotelId, int faciId);
+        Task<Facility_Price_History> FindAllFacilityPriceHistoryByIdAsync(int faciId, int faphId);
+    }
+}
