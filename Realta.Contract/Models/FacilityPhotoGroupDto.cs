208ed6a71f46c409e9a0f22e5680e35e21b925de
alvan <alvan.ghon@gmail.com>
2023-02-28 08:34:30 +0700

    create dto for handle multiple photos

---
+++ Realta.Contract/Models/FacilityPhotoGroupDto.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Realta.Contract.Models
+{
+    public class FacilityPhotoGroupDto
+    {
+        [Required]
+        public FacilityPhotosDto? FacilityPhotos { get; set; }
+
+        public List<IFormFile>? AllPhotos { get; set; }
+    }
+}
