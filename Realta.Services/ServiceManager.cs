c6533fb867c8302eee9ebd56b3bf573a7a7ef253
alvan <alvan.ghon@gmail.com>
2023-03-02 03:45:51 +0700

    create Service Manager for handle Photos and formFile

---
+++ Realta.Services/ServiceManager.cs
@@ -0,0 +1,23 @@
+﻿using Realta.Domain.Base;
+using Realta.Services.Abstraction;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Realta.Services
+{
+    public class ServiceManager : IServiceManager
+    {
+        private readonly Lazy<IFacilityPhotoServices> _lazyFacilityPhotoServices;
+        private readonly Lazy<IUtilityService> _lazyUtilityService;
+
+        public ServiceManager(IRepositoryManager repositoryManager, IUtilityService _lazyUtilityService)
+        {
+            _lazyFacilityPhotoServices = new Lazy<IFacilityPhotoServices>(()=> new FacilityPhotoServices(repositoryManager, _lazyUtilityService));
+        }
+
+        public IFacilityPhotoServices FacilityPhotoServices => throw new NotImplementedException();
+    }
+}
