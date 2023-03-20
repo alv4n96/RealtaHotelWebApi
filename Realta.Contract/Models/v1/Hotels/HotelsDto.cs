53706f5461145ea100c7590ac15676784524bdd8
alvan <alvan.ghon@gmail.com>
2023-03-20 11:13:48 +0700

    fixing Dto for hotels Dto

--- Realta.Contract/Models/v1/Hotels/HotelsDto.cs
+++ Realta.Contract/Models/v1/Hotels/HotelsDto.cs
@@ -6,9 +6,10 @@ namespace Realta.Contract.Models.v1.Hotels
 {
     public class HotelsDto
     {
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
         [Required(ErrorMessage = "hotel id is required")]
         public int HotelId { get; set; }
-
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         [Required(ErrorMessage = "Hotel name is required")]
         public string? HotelName { get; set; }
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
@@ -17,13 +18,14 @@ namespace Realta.Contract.Models.v1.Hotels
         public bool HotelStatus { get; set; }
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public string? HotelReasonStatus { get; set; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
         [AllowNull]
         public decimal HotelRatingStar { get; set; }
-
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
         [Phone]
         [Required(ErrorMessage = "Hotel phone number is required")]
         public string? HotelPhonenumber { get; set; }
-
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
         [AllowNull]
         public DateTime HotelModifiedDate { get; set; }
         [Required(ErrorMessage = "Hotel address is required")]
