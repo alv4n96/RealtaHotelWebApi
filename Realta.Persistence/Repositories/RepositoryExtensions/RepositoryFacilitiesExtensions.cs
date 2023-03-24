065e9e3c3d113bafa4afafa8237352aef565a0e6
alvan <alvan.ghon@gmail.com>
2023-03-24 20:33:54 +0700

    add extensions for facilities search term

--- Realta.Persistence/Repositories/RepositoryExtensions/RepositoryFacilitiesExtensions.cs
+++ Realta.Persistence/Repositories/RepositoryExtensions/RepositoryFacilitiesExtensions.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Text;
-using System.Threading.Tasks;
+using System.Linq.Dynamic.Core;
 using Realta.Domain.Entities;
 
 namespace Realta.Persistence.Repositories.RepositoriesExtensions
@@ -20,35 +17,35 @@ namespace Realta.Persistence.Repositories.RepositoriesExtensions
             return faci.Where(p => p.FaciName.ToLower().Contains(lowerCaseSearchTerm));
         }
 
-        //public static IQueryable<Hotels> Sort(this IQueryable<Hotels> products, string orderByQueryString)
-        //{
-        //    if (string.IsNullOrWhiteSpace(orderByQueryString))
-        //        return products.OrderBy(e => e.HotelName);
+        public static IQueryable<Facilities> Sort(this IQueryable<Facilities> faci, string orderByQueryString)
+        {
+            if (string.IsNullOrWhiteSpace(orderByQueryString))
+                return faci.OrderBy(e => e.FaciName);
 
-        //    var orderParams = orderByQueryString.Trim().Split(',');
-        //    var propertyInfos = typeof(Hotels).GetProperties(BindingFlags.Public | BindingFlags.Instance);
-        //    var orderQueryBuilder = new StringBuilder();
+            var orderParams = orderByQueryString.Trim().Split(',');
+            var propertyInfos = typeof(Facilities).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            var orderQueryBuilder = new StringBuilder();
 
-        //    foreach (var param in orderParams)
-        //    {
-        //        if (string.IsNullOrWhiteSpace(param))
-        //            continue;
+            foreach (var param in orderParams)
+            {
+                if (string.IsNullOrWhiteSpace(param))
+                    continue;
 
-        //        var propertyFromQueryName = param.Split(" ")[0];
-        //        var objectProperty = propertyInfos.FirstOrDefault(pi => pi.Name.Equals(propertyFromQueryName, StringComparison.InvariantCultureIgnoreCase));
+                var propertyFromQueryName = param.Split(" ")[0];
+                var objectProperty = propertyInfos.FirstOrDefault(pi => pi.Name.Equals(propertyFromQueryName, StringComparison.InvariantCultureIgnoreCase));
 
-        //        if (objectProperty == null)
-        //            continue;
+                if (objectProperty == null)
+                    continue;
 
-        //        var direction = param.EndsWith(" desc") ? "descending" : "ascending";
-        //        orderQueryBuilder.Append($"{objectProperty.Name} {direction}, ");
-        //    }
+                var direction = param.EndsWith(" desc") ? "descending" : "ascending";
+                orderQueryBuilder.Append($"{objectProperty.Name} {direction}, ");
+            }
 
-        //    var orderQuery = orderQueryBuilder.ToString().TrimEnd(',', ' ');
-        //    if (string.IsNullOrWhiteSpace(orderQuery))
-        //        return products.OrderBy(e => e.HotelName);
+            var orderQuery = orderQueryBuilder.ToString().TrimEnd(',', ' ');
+            if (string.IsNullOrWhiteSpace(orderQuery))
+                return faci.OrderBy(e => e.FaciName);
 
-        //    return products.OrderBy(orderQuery);
-        //}
+            return faci.OrderBy(orderQuery);
+        }
     }
 }
